0626a53648fbe4be90e18a67987359ec2ced3726
Miles Mickelson <milesmickelson87@gmail.com>
2020-10-12 21:08:00 -0400

    removes unused code and puts scaffolding for project in place

--- src/taskList.js
+++ src/taskList.js
@@ -1,33 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
 const TaskList = () => {
-  const [seconds, setSeconds] = useState(0);
-  const [isActive, setIsActive] = useState(false);
-
-  function toggle() {
-    setIsActive(! isActive);
-  }
-
-  function reset() {
-    setSeconds(0);
-    setIsActive(false);
-  }
-
-  useEffect(() => {
-    let interval = null;
-    if (isActive) {
-      interval = setInterval(() => {
-        setSeconds(seconds => seconds + 1);
-      }, 1000);
-    } else if (! isActive && seconds !== 0) {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [isActive, seconds]);
-
   return (
     <main className='main-container'>
-      <div>Hello</div>
+      <div>Hello, this is your to-do list</div>
     </main>
   );
 };
