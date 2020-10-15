ec465c81bd50fd631a5c708dbfaf5fefc7cd5fd1
Miles Mickelson <milesmickelson87@gmail.com>
2020-10-15 13:59:32 -0400

    updates settings pane switches ids and names and state, no connection to functionality

--- src/settings.js
+++ src/settings.js
@@ -4,7 +4,6 @@ import { purple } from '@material-ui/core/colors';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
-import Divider from '@material-ui/core/Divider';
 
 const PurpleSwitch = withStyles({
   switchBase: {
@@ -22,7 +21,15 @@ const PurpleSwitch = withStyles({
 
 export default function Settings() {
   const [state, setState] = React.useState({
-    checkedA: true,
+    startOnLogin: true,
+    startVisLogin: true,
+    showVisTime: true,
+    showTaskName: true,
+    showTaskTime: true,
+    globalDnd: false,
+    todoDnd: false,
+    visionDnd: false,
+    postureDnd: false,
   });
 
   const handleChange = (event) => {
@@ -34,45 +41,42 @@ export default function Settings() {
       <div id='settings-wrap'>
         <FormGroup>
           <h4 className='em-label'>General</h4>
-          <Divider className='divider' />
           <FormControlLabel
-            control={ <PurpleSwitch checked={ state.checkedA } onChange={ handleChange } name='checkedA' id='general-settings' /> }
+            control={ <PurpleSwitch checked={ state.startOnLogin } onChange={ handleChange } name='startOnLogin' id='startOnLogin' /> }
             label='Start Work Rhythm on Login'
           />
           <FormControlLabel
-            control={ <PurpleSwitch checked={ state.checkedA } onChange={ handleChange } name='checkedA' id='general-settings' /> }
+            control={ <PurpleSwitch checked={ state.startVisLogin } onChange={ handleChange } name='startVisLogin' id='startVisLogin' /> }
             label='Start vision break interval on Login'
           />
           <h4 className='em-label'>Display</h4>
-          <Divider className='divider' />
           <FormControlLabel
-            control={ <PurpleSwitch checked={ state.checkedA } onChange={ handleChange } name='checkedA' id='general-settings' /> }
+            control={ <PurpleSwitch checked={ state.showVisTime } onChange={ handleChange } name='showVisTime' id='showVisTime' /> }
             label='Show vision break time remaining in toolbar'
           />
           <FormControlLabel
-            control={ <PurpleSwitch checked={ state.checkedA } onChange={ handleChange } name='checkedA' id='general-settings' /> }
+            control={ <PurpleSwitch checked={ state.showTaskName } onChange={ handleChange } name='showTaskName' id='showTaskName' /> }
             label='Show current task name in toolbar'
           />
           <FormControlLabel
-            control={ <PurpleSwitch checked={ state.checkedA } onChange={ handleChange } name='checkedA' id='general-settings' /> }
+            control={ <PurpleSwitch checked={ state.showTaskTime } onChange={ handleChange } name='showTaskTime' id='showTaskTime' /> }
             label='Show current task time in toolbar'
           />
-          <h4 className='em-label'>Do Not Disturb</h4>
-          <Divider className='divider' />
+          <h4 className='underline'>Do Not Disturb</h4>
           <FormControlLabel
-            control={ <PurpleSwitch checked={ state.checkedA } onChange={ handleChange } name='checkedA' id='general-settings' /> }
+            control={ <PurpleSwitch checked={ state.globalDnd } onChange={ handleChange } name='globalDnd' id='globalDnd' /> }
             label='Turn off all timers and notifications'
           />
           <FormControlLabel
-            control={ <PurpleSwitch checked={ state.checkedA } onChange={ handleChange } name='checkedA' id='general-settings' /> }
+            control={ <PurpleSwitch checked={ state.todoDnd } onChange={ handleChange } name='todoDnd' id='todoDnd' /> }
             label='Turn off all to-do list timers and notifications'
           />
           <FormControlLabel
-            control={ <PurpleSwitch checked={ state.checkedA } onChange={ handleChange } name='checkedA' id='general-settings' /> }
+            control={ <PurpleSwitch checked={ state.visionDnd } onChange={ handleChange } name='visionDnd' id='visionDnd' /> }
             label='Turn off all vision interval timers and notifications'
           />
           <FormControlLabel
-            control={ <PurpleSwitch checked={ state.checkedA } onChange={ handleChange } name='checkedA' id='general-settings' /> }
+            control={ <PurpleSwitch checked={ state.postureDnd } onChange={ handleChange } name='postureDnd' id='postureDnd' /> }
             label='Turn off all postural interval timers and notifications'
           />
         </FormGroup>
