e181cd4db1a2685cf516cbaa8141e85c6b3f6cea
Miles Mickelson <milesmickelson87@gmail.com>
2020-11-03 17:17:52 -0500

    places navigation bar into its own file and removes unnecessary dependencies

--- src/app.js
+++ src/app.js
@@ -1,15 +1,5 @@
 import React from 'react';
 
-import PropTypes from 'prop-types';
-
-// import {
-//   HashRouter as Router,
-//   Route,
-//   Switch
-// } from 'react-router-dom';
-
-import { MemoryRouter as Router } from 'react-router';
-
 import {
   makeStyles,
   ThemeProvider,
@@ -17,133 +7,32 @@ import {
 } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-
-import CheckIcon from '@material-ui/icons/CheckCircleOutline';
-import VisionIcon from '@material-ui/icons/Visibility';
-import PostureIcon from '@material-ui/icons/AccessibilityNew';
-import SettingsIcon from '@material-ui/icons/Settings';
 import theme from './theme';
 
-import Workflow from './taskList';
-import Vision from './visiontimer';
-import Posture from './standTimer';
-import Settings from './settings';
+import NavBar from './navbar';
 
 // setConfig({
 //   showReactDomPatchNotification: false
 // });
 
-export default function App() {
-
-  function TabPanel(props) {
-    const {
-      children, value, index, ...other
-    } = props;
-    return (
-      <div
-        role='tabpanel'
-        hidden={ value !== index }
-        id={ `nav-tabpanel-${index}` }
-        aria-labelledby={ `nav-tab-${index}` }
-        {...other}
-      >
-        {value === index && (
-          <Box p={ 4 }>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-
-  TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
-  };
-
-  function a11yProps(index) {
-    return {
-      id: `nav-tab-${index}`,
-      'aria-controls': `nav-tabpanel-${index}`,
-    };
-  }
-
-  function LinkTab(props) {
-    return (
-      <Tab
-        component='a'
-        onClick={ (event) => {
-          event.preventDefault();
-        } }
-        {...props}
-      />
-    );
-  }
+const App = () => {
   const useStyles = makeStyles(() => ({
     appContainer: {
       background: 'linear-gradient(130deg, #B2D1DD 0%, #406371 100%)',
       height: '100%',
       maxWidth: '100%',
     },
-    navPaper: {
-      width: '100%',
-      maxWidth: 960,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-    root: {
-      flexGrow: 1,
-    },
   }));
 
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
   return (
     <ThemeProvider theme={ theme }>
       <Container className={ classes.appContainer } disableGutters>
         <CssBaseline />
-        <div className={ classes.navPaper }>
-          <AppBar position='static'>
-            <Tabs
-              variant='fullWidth'
-              value={ value }
-              onChange={ handleChange }
-              aria-label='nav tabs example'
-            >
-              <LinkTab icon={ <CheckIcon /> } label='Workflow' href='/Workflow' {...a11yProps(0)} />
-              <LinkTab icon={ <VisionIcon /> } label='Vision' href='/Vision' {...a11yProps(1)} />
-              <LinkTab icon={ <PostureIcon /> } label='Posture' href='/Posture' {...a11yProps(2)} />
-              <LinkTab icon={ <SettingsIcon /> } label='Settings' href='/Settings' {...a11yProps(3)} />
-            </Tabs>
-            <Router>
-              <TabPanel value={ value } index={ 0 }>
-                <Workflow />
-              </TabPanel>
-              <TabPanel value={ value } index={ 1 }>
-                <Vision />
-              </TabPanel>
-              <TabPanel value={ value } index={ 2 }>
-                <Posture />
-              </TabPanel>
-              <TabPanel value={ value } index={ 3 }>
-                <Settings />
-              </TabPanel>
-            </Router>
-          </AppBar>
-        </div>
+        <NavBar />
       </Container>
     </ThemeProvider>
   );
-}
+};
+
+export default App;
--- src/navbar.js
+++ src/navbar.js
@@ -1,26 +1,32 @@
-import React, { useState, useEffect } from 'react';
-
+import React from 'react';
 import PropTypes from 'prop-types';
+import { MemoryRouter as Router } from 'react-router';
+
+import {
+  makeStyles,
+  withStyles
+} from '@material-ui/core/styles';
 
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 import CheckIcon from '@material-ui/icons/CheckCircleOutline';
 import VisionIcon from '@material-ui/icons/Visibility';
 import PostureIcon from '@material-ui/icons/AccessibilityNew';
 import SettingsIcon from '@material-ui/icons/Settings';
+
 import Workflow from './taskList';
 import Vision from './visiontimer';
 import Posture from './standTimer';
 import Settings from './settings';
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
+  const {
+    children, value, index, ...other
+  } = props;
   return (
     <div
       role='tabpanel'
@@ -58,12 +64,16 @@ function LinkTab(props) {
       onClick={ (event) => {
         event.preventDefault();
       } }
-      { ...props }
+      {...props}
     />
   );
 }
-
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
+  appContainer: {
+    background: 'linear-gradient(130deg, #B2D1DD 0%, #406371 100%)',
+    height: '100%',
+    maxWidth: '100%',
+  },
   navPaper: {
     width: '100%',
     maxWidth: 960,
@@ -84,19 +94,22 @@ const NavBar = () => {
   };
 
   return (
-    <div className={ classes.navPaper }>
+    <>
       <AppBar position='static'>
         <Tabs
           variant='fullWidth'
           value={ value }
           onChange={ handleChange }
           aria-label='nav tabs example'
+          className={ classes.navPaper }
         >
-          <LinkTab icon={ <CheckIcon /> } label='Workflow' component={ Workflow } to='/Workflow' {...a11yProps(0)} />
-          <LinkTab icon={ <VisionIcon /> } label='Vision' component={ Vision } to='/Vision' {...a11yProps(1)} />
-          <LinkTab icon={ <PostureIcon /> } label='Posture' component={ Posture } to='/Posture' {...a11yProps(2)} />
-          <LinkTab icon={ <SettingsIcon /> } label='Settings' component={ Settings } to='/Settings' {...a11yProps(3)} />
+          <LinkTab icon={ <CheckIcon /> } label='Workflow' href='/Workflow' {...a11yProps(0)} />
+          <LinkTab icon={ <VisionIcon /> } label='Vision' href='/Vision' {...a11yProps(1)} />
+          <LinkTab icon={ <PostureIcon /> } label='Posture' href='/Posture' {...a11yProps(2)} />
+          <LinkTab icon={ <SettingsIcon /> } label='Settings' href='/Settings' {...a11yProps(3)} />
         </Tabs>
+      </AppBar>
+      <Router>
         <TabPanel value={ value } index={ 0 }>
           <Workflow />
         </TabPanel>
@@ -109,97 +122,9 @@ const NavBar = () => {
         <TabPanel value={ value } index={ 3 }>
           <Settings />
         </TabPanel>
-      </AppBar>
-    </div>
+      </Router>
+    </>
   );
 };
 
 export default NavBar;
-
-// const NavBar = (props) => {
-//   const classes = useStyles();
-// const [value, setValue] = React.useState(0);
-
-// const handleChange = (event, newValue) => {
-//   setValue(newValue);
-// };
-
-// const handleCallToRouter = (value) => {
-//   this.props.history.push(value);
-// };
-
-//   return (
-//     <Router>
-//       <Paper round elevation={ 3 } variant='outlined fullWidth' className={ classes.navPaper }>
-//         <Tabs
-//           variant='fullWidth'
-//           // value={ value }
-//           // onChange={ handleChange }
-//           value={ value }
-//           onChange={ handleCallToRouter }
-//           indicatorColor='secondary'
-//           textColor='secondary'
-//           aria-label='icon label tabs example'
-//           centered
-//         >
-//           <Tab icon={ <CheckIcon fontSize='large' /> } label='Workflow' value='/Workflow'>
-//             <Route exact path='/Workflow' component={ Workflow } />
-//           </Tab>
-//           <Tab icon={ <VisionIcon fontSize='large' /> } label='Vision' value='/Vision'>
-//             <Route exact path='/Vision' component={ Vision } />
-//           </Tab>
-//           <Tab icon={ <PostureIcon fontSize='large' /> } label='Posture' value='/Posture'>
-//             <Route exact path='/Posture' component={ Posture } />
-//           </Tab>
-//           <Tab icon={ <SettingsIcon fontSize='large' /> } label='Settings' value='/Settings'>
-//             <Route exact path='/Settings' component={ Settings } />
-//           </Tab>
-//         </Tabs>
-//       </Paper>
-//     </Router>
-//   );
-// };
-
-// portfolio Navigation
-// export default class NavBar extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       home: true,
-//     };
-//     this.toggleHome = this.toggleHome.bind(this);
-//   }
-
-//   toggleHome() {
-//     this.setState({
-//       home: true,
-//       projects: false,
-//       credentials: false,
-//       about: false,
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div className='container-A'>
-//         <div className='container-B'>
-//           <nav id='nav-wrap' aria-label='primary'>
-//             <a
-//               title='Home Page'
-//               href='#/'
-//               rel='noopener noreferrer'
-//               onClick={ this.toggleHome }
-//             >
-//               <img
-//                 className={ `nav-icon ${this.state.home ? 'isSpinning' : 'notSpinning'}` }
-//                 id='wheel'
-//                 src={ wheel }
-//                 alt='Home'
-//               />
-//             </a>
-//           </nav>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
