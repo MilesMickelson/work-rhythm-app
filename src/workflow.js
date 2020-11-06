e0c3838ccdb82f45c182ee8c61e1ace505e2fd64
Miles Mickelson <milesmickelson87@gmail.com>
2020-11-05 19:44:42 -0500

    saving wip on implementation of MatUI table grid

--- src/workflow.js
+++ src/workflow.js
@@ -22,30 +22,22 @@ import Switch from '@material-ui/core/Switch';
 import DeleteIcon from '@material-ui/icons/Delete';
 import FilterListIcon from '@material-ui/icons/FilterList';
 
-function createData(name, calories, fat, carbs, protein) {
+function createData(title, priority, recurring, time, due) {
   return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein
+    title,
+    priority,
+    recurring,
+    time,
+    due
   };
 }
 
 const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
+  createData('Write more code', 'Very High', 'Everyday', '11-1-2021', 4.3),
+  createData('Eat healthy', 'High', 'Weekly', '11-2-2021', 4.9),
+  createData('Cook dinner', 'Medium', 'Business Days', '11-3-2023', 6.0),
+  createData('Exercise', 'Medium', 'Bi-Weekly', '12-4-2021', 4.0),
+  createData('Call mom', 'Low', 'Monthly', '11-7-2029', 3.9),
 ];
 
 function descendingComparator(a, b, orderBy) {
@@ -76,34 +68,34 @@ function stableSort(array, comparator) {
 
 const headCells = [
   {
-    id: 'name',
+    id: 'title',
     numeric: false,
     disablePadding: true,
-    label: 'Dessert (100g serving)'
+    label: 'Title'
   },
   {
-    id: 'calories',
-    numeric: true,
+    id: 'priority',
+    numeric: false,
     disablePadding: false,
-    label: 'Calories'
+    label: 'Priority'
   },
   {
-    id: 'fat',
-    numeric: true,
+    id: 'recurring',
+    numeric: false,
     disablePadding: false,
-    label: 'Fat (g)'
+    label: 'Recurring'
   },
   {
-    id: 'carbs',
+    id: 'due',
     numeric: true,
     disablePadding: false,
-    label: 'Carbs (g)'
+    label: 'Due'
   },
   {
-    id: 'protein',
+    id: 'time',
     numeric: true,
     disablePadding: false,
-    label: 'Protein (g)'
+    label: 'Time Remaining'
   },
 ];
 
@@ -255,7 +247,7 @@ const useStyles = makeStyles((theme) => ({
 const WorkFlow = () => {
   const classes = useStyles();
   const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('calories');
+  const [orderBy, setOrderBy] = React.useState('priority');
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
@@ -269,19 +261,19 @@ const WorkFlow = () => {
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.name);
+      const newSelecteds = rows.map((n) => n.title);
       setSelected(newSelecteds);
       return;
     }
     setSelected([]);
   };
 
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
+  const handleClick = (event, title) => {
+    const selectedIndex = selected.indexOf(title);
     let newSelected = [];
 
     if (selectedIndex === - 1) {
-      newSelected = newSelected.concat(selected, name);
+      newSelected = newSelected.concat(selected, title);
     } else if (selectedIndex === 0) {
       newSelected = newSelected.concat(selected.slice(1));
     } else if (selectedIndex === selected.length - 1) {
@@ -308,7 +300,7 @@ const WorkFlow = () => {
     setDense(event.target.checked);
   };
 
-  const isSelected = (name) => selected.indexOf(name) !== - 1;
+  const isSelected = (title) => selected.indexOf(title) !== - 1;
 
   const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
@@ -336,17 +328,16 @@ const WorkFlow = () => {
               {stableSort(rows, getComparator(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
-                  const isItemSelected = isSelected(row.name);
+                  const isItemSelected = isSelected(row.title);
                   const labelId = `enhanced-table-checkbox-${index}`;
-
                   return (
                     <TableRow
                       hover
-                      onClick={ (event) => handleClick(event, row.name) }
+                      onClick={ (event) => handleClick(event, row.title) }
                       role='checkbox'
                       aria-checked={ isItemSelected }
                       tabIndex={ - 1 }
-                      key={ row.name }
+                      key={ row.title }
                       selected={ isItemSelected }
                     >
                       <TableCell padding='checkbox'>
@@ -356,12 +347,12 @@ const WorkFlow = () => {
                         />
                       </TableCell>
                       <TableCell component='th' id={ labelId } scope='row' padding='none'>
-                        {row.name}
+                        {row.title}
                       </TableCell>
-                      <TableCell align='right'>{row.calories}</TableCell>
-                      <TableCell align='right'>{row.fat}</TableCell>
-                      <TableCell align='right'>{row.carbs}</TableCell>
-                      <TableCell align='right'>{row.protein}</TableCell>
+                      <TableCell align='right'>{row.priority}</TableCell>
+                      <TableCell align='right'>{row.recurring}</TableCell>
+                      <TableCell align='right'>{row.time}</TableCell>
+                      <TableCell align='right'>{row.due}</TableCell>
                     </TableRow>
                   );
                 })}
@@ -385,7 +376,7 @@ const WorkFlow = () => {
       </Paper>
       <FormControlLabel
         control={ <Switch checked={ dense } onChange={ handleChangeDense } /> }
-        label='Dense padding'
+        label='Condense'
       />
     </div>
   );
